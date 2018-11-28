7432001dad88114fdc2b9418dd1f545444ebc638
James Newton-King <james@newtonking.com>
2018-11-28 13:37:22 +1300

    Move EndpointModel and IEndpointConventionBuilder to HTTP abstractions and rename

--- src/Routing/samples/RoutingSandbox/AuthorizationMiddleware/EndpointConventionBuilderExtensions.cs
+++ src/Routing/samples/RoutingSandbox/AuthorizationMiddleware/EndpointConventionBuilderExtensions.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.AspNetCore.Routing;
@@ -10,7 +10,7 @@ namespace Microsoft.AspNetCore.Builder
     {
         public static IEndpointConventionBuilder RequireAuthorization(this IEndpointConventionBuilder builder, params string[] roles)
         {
-            builder.Apply(endpointBuilder => endpointBuilder.Metadata.Add(new AuthorizationMetadata(roles)));
+            builder.Add(endpointBuilder => endpointBuilder.Metadata.Add(new AuthorizationMetadata(roles)));
             return builder;
         }
     }
